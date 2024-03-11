35262401c302ebd1f9bf0ab4b99dd36da0d41391
Mojtaba Vahabnezhad <mojtabavahabnezhad@gmail.com>
2024-03-11 22:40:06 +0330

    code the whole program :/

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch
+
+first put a promt at first so when you enter the page it asks you for the number of columns and rows
--- script.js
+++ script.js
@@ -1,16 +1,64 @@
-let numbers = 16;
+const container = document.querySelector(".divContainer");
 const newBut = document.querySelector(".new");
+let numbers;
+const resetBut = document.querySelector(".reset");
 
-makeGrid(numbers);
+while(true){
+    numbers = +(prompt("Enter the number of columns and rows: "));
+    if (numbers > 100){
+        alert("Error! Your number is more than 100")
+    } else {
+        break;
+    }
+}
+makeGrid();
 
-newBut.addEventListener("click", (e) => {
-    let newNumb = prompt("Enter the number of rows and columns: ");
-    numbers = +(newNumb);
-    makeGrid(numbers);
+newBut.addEventListener("click", () => {
+    while(true){
+        numbers = +(prompt("Enter the number of columns and rows: "));
+        if (numbers > 100){
+            alert("Error! Your number is more than 100")
+        } else {
+            break;
+        }
+    }
+    const boxes = document.querySelectorAll(".boxContainer");
+    boxes.forEach((boxe) => {
+        container.removeChild(boxe);
+    })
+    makeGrid();
+
 })
 
-function makeGrid(number){
-    let length = (840 - number)/number;
+function makeGrid(){
+    let nums = numbers;
 
-
+    for(let i = 0; i < nums ; ++i){
+        const boxContainer = document.createElement("div");
+        boxContainer.setAttribute("class", "boxContainer");
+        for(let j = 0 ; j < nums ; ++j){
+            const box = document.createElement("div");
+            box.setAttribute("class", "box");
+            box.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = randomColor();
+            })
+            boxContainer.appendChild(box);
+        }
+        container.appendChild(boxContainer);
+    }
 }
+
+function randomColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`
+}
+
+resetBut.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".boxContainer");
+    boxes.forEach((boxe) => {
+        container.removeChild(boxe);
+    })
+    makeGrid();
+})
